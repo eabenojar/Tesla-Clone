64caeb5b88d7d97ed8d3df092f18abc53289b3d0
Eric Abenojar <abenoeric@gmail.com>
2018-05-02 13:21:36 -0700

    First update for tesla clone

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+//APP CONFIGURATION
+//SET VIEW ENGINE FOR NODE.JS
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req,res){
+  res.render("index");
+});
+
+app.get("/models", function(req,res){
+  res.render("modelS")
+})
+
+app.get("/model3", function(req,res){
+  res.render("model3");
+})
+
+app.get("/modelx", function(req,res){
+  res.render("modelX");
+})
+
+app.get("/roadster", function(req,res){
+  res.render("roadster");
+})
+
+app.listen(3000, console.log("APP STARTED ON PORT 3000"));
